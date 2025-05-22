67801ce8df04d24d21d4b5ea7868081f594afe01
xanderpacquet <xander.pacquet@student.tsaam.be>
2025-05-22 11:44:53 +0200

    eerste commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,21 @@
+const TodoList = [
+  {
+    id: 1,
+    title: "was doen",
+  },
+  {
+    id: 2,
+    title: "studeren",
+  },
+  {
+    id: 3,
+    title: "eten",
+  },
+];
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
+const filterWinkels = document.getElementById("filter");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -15,3 +30,28 @@ form.addEventListener("submit", function (e) {
     input.value = "";
   }
 });
+
+TodoList.forEach((toDo) => {
+  const selectItem = document.createElement("option");
+  selectItem.value = toDo.id;
+  selectItem.innerHTML = toDo.title;
+  list.appendChild(selectItem);
+});
+
+const dropdown = document.querySelector("#winkels");
+
+dropdown.addEventListener("change", () => {
+  document.body.style.backgroundColor = dropdown.value;
+});
+
+function showTaken(TodoList, htmlElement) {
+  htmlElement.innerHTML = "";
+  TodoList.forEach((user) => {
+    let newToDo = document.createElement("li");
+    newToDo.innerHTML = user.title;
+    if (TaakGedaan(title)) {
+      newToDo.classList.add("dubbelUser");
+    }
+    htmlElement.appendChild(newUserLi);
+  });
+}
